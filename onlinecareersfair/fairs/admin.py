ae3c76ec1107cc34904d3eb472fa96e0ce7191f2
ThorntonChan <thorntonchan@gmail.com>
2021-07-11 19:34:50 +1000

    install django rest framework with swagger & redoc

--- onlinecareersfair/fairs/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models her
-admin.site.register(fairEvent)
-admin.site.register(stall)
--- onlinecareersfair/fairs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FairsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fairs'
