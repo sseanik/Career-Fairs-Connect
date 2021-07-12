8975b1d0bd58f30f699bb3ef68e4767fb5fc94fc
Max Kennedy <max.kennedy03@gmail.com>
2021-07-12 19:42:33 +1000

    home page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import { ChakraProvider } from "@chakra-ui/react"
 import { Route, Switch } from "react-router-dom"
-import Home from './pages/home';
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <ChakraProvider>
       <Switch>
         <Route path="/" component={Home} exact/>
+        <Route path="/login" component={Login} exact />
+        <Route path="/register" component={Register} exact/>
       </Switch>
     </ChakraProvider>
   );
