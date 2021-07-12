9886ccf1efe9bb3188862829a5ebbf092b3495c4
Max Kennedy <max.kennedy03@gmail.com>
2021-07-12 17:42:11 +1000

    Navbar created

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,23 @@
+import React, { } from 'react'
+import { Flex, Spacer, Link } from "@chakra-ui/react"
+
+function Navbar() {
+  return (
+    <Flex id="menu" bg="#2F303A" w="100%" h="80px" color="white" textAlign="center" margin="auto" fontSize="xl">
+      <Link margin="auto" paddingLeft="4%" href="/">
+        Company Logo/Name
+      </Link>
+      <Spacer />
+      <Flex margin="auto" paddingRight="4%">
+        <Link paddingRight="50px" href="/login">
+          Login
+        </Link>
+        <Link href="/register">
+          Join
+        </Link>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Navbar
