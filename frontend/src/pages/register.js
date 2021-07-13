650b0ba314b358f029642b07aad31b15a3e5e763
Max Kennedy <max.kennedy03@gmail.com>
2021-07-13 19:58:04 +1000

    Changed Links to using react-router-dom

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { ChakraProvider, Flex, Link, Box, Button } from "@chakra-ui/react"
+import { ChakraProvider, Flex, Box, Button, Link as ChakraLink } from "@chakra-ui/react"
+import { Link } from 'react-router-dom'
 
 function Register() {
   return (
     <ChakraProvider>
       <Flex id="menu" w="100%" bg="#E5E5E5"  h="8vh" color="white" fontSize="xl">
-        <Link mt="auto" mb="auto" paddingLeft="4%" href="/" color="#2F303A">
-          Company Logo/Name
-        </Link>
+          <Box mt="auto" mb="auto" paddingLeft="4%" color="#2F303A">
+            <ChakraLink>
+              <Link to="/">
+                Company Logo/Name
+              </Link>
+            </ChakraLink>
+          </Box>
       </Flex>
       <Flex width="100%" height="92vh" bg="#E5E5E5">
         <Box width="50%" m="auto" textAlign="center">
@@ -18,21 +23,21 @@ function Register() {
               Join Now
             </Box>
             <Box>
-              <Link href="/register/student">
+              <Link to="/register/student">
                 <Button mt="12%" fontSize="2xl" padding="28px" color="white" bgColor="#516C7B" width="70%" fontWeight="medium">
                   I am a student
                 </Button>
               </Link>
             </Box>
             <Box>
-              <Link href="/register/employer">
+              <Link to="/register/employer">
                 <Button mt="8%" fontSize="2xl" padding="28px" color="white" bgColor="#516C7B" width="70%" fontWeight="medium">
                   I am an employer
                 </Button>
               </Link>
             </Box>
             <Box>
-              <Link href="/register/university">
+              <Link to="/register/university">
                 <Button mt="8%" mb="8%" fontSize="2xl" padding="28px" color="white" bgColor="#516C7B" width="70%" fontWeight="medium">
                   I am a university
                 </Button>
