eae47da0541290d4ea0bc6f69bfadf7ed8e3098d
YuliaRodionov <62008245+YuliaRodionov@users.noreply.github.com>
2021-07-16 21:43:53 +1000

    Database setup  (#3)

--- src/api/settings.py
+++ src/api/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CareerFairDB',
+        'USER': 'postgres',
+        'PASSWORD': 'DPhgwV3xpFGHg8K',
+        'HOST': 'comp9323.cruw92ayeie9.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
