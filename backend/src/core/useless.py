4bfe2b16f16232031227842492c04117e26cf2fc
AF <arthur.fung@student.unsw.edu.au>
2021-07-17 20:05:35 +1000

    working

--- backend/src/core/useless.py
+++ backend/src/core/useless.py
@@ -1,47 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+# Create your models here.
+
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, first_name, last_name, user_type, password=None):
+    def create_user(self, email, first_name, last_name, username, password, is_university, is_company, is_student):
         if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
+            raise ValueError("users must have a email")
         if not first_name:
-            raise ValueError('Users must have a first_name')
+            raise ValueError("users must have a first_name")
         if not last_name:
-            raise ValueError('Users must have a last_name')
+            raise ValueError("users must have a last_name")
+        if not username:
+            raise ValueError("users must have a username")
 
         user = self.model(
             email=self.normalize_email(email),
             username=username,
             first_name=first_name,
             last_name=last_name,
-            user_type=user_type,
+            is_university=is_university,
+            is_company=is_company,
+            is_student=is_student,
+            password=password
         )
-
-        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, first_name, last_name, password, user_type):
+    def create_superuser(self, email, first_name, last_name, username, password, is_university, is_company, is_student):
         if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
+            raise ValueError("users must have a email")
         if not first_name:
-            raise ValueError('Users must have a first_name')
+            raise ValueError("users must have a first_name")
         if not last_name:
-            raise ValueError('Users must have a last_name')
+            raise ValueError("users must have a last_name")
+        if not username:
+            raise ValueError("users must have a username")
 
         user = self.create_user(
             email=self.normalize_email(email),
-            password=password,
             username=username,
             first_name=first_name,
             last_name=last_name,
-            user_type=user_type,
+            is_university=is_university,
+            is_company=is_company,
+            is_student=is_student,
+            password=password
         )
         user.is_admin = True
         user.is_staff = True
@@ -51,30 +56,33 @@ class MyAccountManager(BaseUserManager):
 
 
 class Account(AbstractBaseUser):
-    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    # default required
     username = models.CharField(max_length=30, unique=True)
     date_joined = models.DateTimeField(
         verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    user_type = models.IntegerField(
-        choices=((0, 'university'), (1, 'student'), (2, 'company')), default=0)
+    # additional fields
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    # student, admin, company
+    is_university = models.BooleanField(default=False)
+    is_company = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name',
-                       'last_name', 'user_type']
+    REQUIRED_FIELDS = ['first_name', 'last_name',
+                       'username', 'is_university', 'is_company', 'is_student']
 
     objects = MyAccountManager()
 
     def __str__(self):
         return self.email
 
-    # For checking permissions. to keep it simple all admin have ALL permissons
+        # For checking permissions. to keep it simple all admin have ALL permissons
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
