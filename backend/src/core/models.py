972fe2f0f6e744b87dfcfe455849496715248d36
AF <arthur.fung@student.unsw.edu.au>
2021-07-17 18:43:32 +1000

    stuff

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 
@@ -62,8 +61,8 @@ class Account(AbstractBaseUser):
     date_joined = models.DateTimeField(
         verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     first_name = models.CharField(max_length=50)
@@ -83,9 +82,11 @@ class Account(AbstractBaseUser):
     def __str__(self):
         return self.email
 
+        # For checking permissions. to keep it simple all admin have ALL permissons
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
+    # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
     def has_module_perms(self, app_label):
         return True
 
