49afec20208ad6aa9c57103253894017c08b49f2
YuliaRodionov <rodionov.yulia@gmail.com>
2021-07-17 22:04:10 +1000

    Fixed models. Cleared old migrations and applied new models to DB.

--- src/core/models.py
+++ src/core/models.py
@@ -13,33 +13,57 @@ class Companies(models.Model):
     company_description = models.TextField()
     company_webpage_url = models.CharField(max_length = 150)
     company_logo_url = models.CharField(max_length = 150)
-    
+
+
+class Universities(models.Model):
+    university_id = models.AutoField(primary_key = True)
+    university_name = models.CharField(max_length=50)
+    # Potential cause of issues on deleting
+    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
+
+
+class Events(models.Model):
+    event_id = models.AutoField(primary_key = True)
+    # Discuss Max Text and Char???
+    university_id = models.ForeignKey(Universities, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 50)
+    company_description = models.TextField()
+    # default auto_now=False, auto_now_add=False
+    event_start_time = models.TimeField()
+    event_end_time = models.TimeField()
+
+
 class Registrations(models.Model):
     event_id = models.ForeignKey(Events, unique = False, on_delete = models.RESTRICT)
     company_id = models.ForeignKey(Companies, unique = False, on_delete = models.CASCADE)
     approval_status = models.CharField(max_length = 8)
     class Meta:
         unique_together = (("event_id", "company_id"),)
-    
-class Events(models.Model):
-    event_id = models.AutoField(primary_key = True)
-    # Discuss Max Text and Char???
-    university_id = models.ForeignKey(Universities, on_delete=models.CASCADE)(max_length = 50)
-    title = models.CharField(max_length = 50)
-    company_description = models.TextField()
-    # default auto_now=False, auto_now_add=False
-    event_start_time = models.TimeField()
-    event_end_time = models.TimeField()
-class Universities(models.Model):
-    university_id = models.AutoField(primary_key = True)
-    university_name = models.CharField(max_length=50)
-    # Potential cause of issues on deleting
-    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
-class Stall(models.Model):
+
+
+class Stalls(models.Model):
     stall_id = models.AutoField(primary_key = True) 
     company_id = models.ForeignKey(Companies, on_delete=models.CASCADE)
     event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
 
+
 class Presentations(models.Model):
     presentation_id = models.AutoField(primary_key = True)
     presentation_link = models.CharField(max_length=255)
+
+
+class Students(models.Model):
+    student_id = models.AutoField(primary_key = True)
+    university = models.CharField(max_length = 50)
+    degree = models.CharField(max_length = 100)
+    wam = models.DecimalField(max_digits=5, decimal_places=2)
+    user_id = models.ForeignKey(User, unique = True, on_delete = models.RESTRICT)
+
+
+class Opportunities(models.Model):
+    job_id = models.AutoField(primary_key = True)
+    job_description = models.TextField()
+    application_link = models.CharField(max_length = 100)
+    stall_id = models.ForeignKey(Stalls, unique = True, on_delete = models.RESTRICT)
+
+
