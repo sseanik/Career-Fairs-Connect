8e4c42e1b1ee0efe1a9a5ee564c779f2bca35921
AF <arthur.fung@student.unsw.edu.au>
2021-07-18 18:41:09 +1000

    rego done

--- src/core/views.py
+++ src/core/views.py
@@ -14,14 +14,13 @@ def register_student(request):
     print(request.data)
     if request.method == "POST":
         user_serializer = UserSerializer(user, data=request.data, fields=('email', 'password', 'first_name', 'last_name'))
-        if user_serializer.is_valid():
+        student = Students(user_id=user)
+        student_serializer = StudentSerializer(student, data=request.data, fields=('university',))
+        if user_serializer.is_valid() and student_serializer.is_valid():
             user_serializer.save()
-            student = Students(user_id=user)
-            student_serializer = StudentSerializer(student, data=request.data, fields=('university',))
-            if student_serializer.is_valid():
-                student_serializer.save()
+            student_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
 
-                return Response(user_serializer.data, status=status.HTTP_201_CREATED)
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST', ])
@@ -30,14 +29,13 @@ def register_university(request):
     print(request.data)
     if request.method == "POST":
         user_serializer = UserSerializer(user, data=request.data, fields=('email', 'password',))
-        if user_serializer.is_valid():
+        university = Universities(user_id=user)
+        university_serializer = UniversitySerializer(university, data=request.data, fields=('university_name', 'university_abbreviation', 'university_logo_url'))
+        if user_serializer.is_valid() and university_serializer.is_valid():
             user_serializer.save()
-            university = Universities(user_id=user)
-            university_serializer = UniversitySerializer(university, data=request.data, fields=('university_name', 'university_abbreviation', 'university_logo_url'))
-            if university_serializer.is_valid():
-                university_serializer.save()
+            university_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
 
-                return Response(user_serializer.data, status=status.HTTP_201_CREATED)
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST', ])
@@ -46,12 +44,11 @@ def register_company(request):
     print(request.data)
     if request.method == "POST":
         user_serializer = UserSerializer(user, data=request.data, fields=('email', 'password'))
-        if user_serializer.is_valid():
+        company = Companies(user_id=user)
+        company_serializer = CompanySerializer(company, data=request.data, fields=('company_name', 'company_description', 'company_webpage_url', 'company_logo_url'))
+        if user_serializer.is_valid() and company_serializer.is_valid():
             user_serializer.save()
-            company = Companies(user_id=user)
-            company_serializer = CompanySerializer(company, data=request.data, fields=('company_name', 'company_description', 'company_webpage_url', 'company_logo_url'))
-            if company_serializer.is_valid():
-                company_serializer.save()
+            company_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
 
-                return Response(user_serializer.data, status=status.HTTP_201_CREATED)
         return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
