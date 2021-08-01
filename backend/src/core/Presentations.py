13f71321f91d804bfe262653f69d9a76bc7c7b61
AF <arthur.fung@student.unsw.edu.au>
2021-08-01 22:23:21 +1000

    models updated, reviewed with team

--- backend/src/core/Presentations.py
+++ backend/src/core/Presentations.py
@@ -39,12 +39,12 @@ def get_presentation(request, stallId):
         presentation = Presentations.objects.get(stall_id=stallId)
     except:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    presentation_serializer = PresentationSerializer(presentation, fields=('stall_id','datetime', 'presentation_link', 'presentation_description'))
+    presentation_serializer = PresentationSerializer(presentation, fields=('stall_id','start_time','end_time', 'presentation_link', 'presentation_description'))
     return Response(presentation_serializer.data, status=status.HTTP_201_CREATED)   
 
 @api_view(['POST', ])
 def create_presentation(request):
-    presentation_serializer = PresentationSerializer(data=request.data, fields=('stall_id','datetime', 'presentation_link', 'presentation_description'))
+    presentation_serializer = PresentationSerializer(data=request.data, fields=('stall_id','start_time','end_time', 'presentation_link', 'presentation_description'))
     
     if not presentation_serializer.is_valid():
         return Response(presentation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -57,7 +57,7 @@ def edit_presentation(request):
         presentation = Presentations.objects.get(stall_id=request.data['stall_id'])
     except:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    presentation_serializer = PresentationSerializer(presentation, data=request.data, fields=('stall_id','datetime', 'presentation_link', 'presentation_description'))
+    presentation_serializer = PresentationSerializer(presentation, data=request.data, fields=('stall_id','start_time','end_time', 'presentation_link', 'presentation_description'))
     if not presentation_serializer.is_valid():
         return Response(presentation_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     presentation_serializer.save()
