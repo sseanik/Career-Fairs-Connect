68397c894c7d410ba0c29d328379ebd24b2e998f
Wanchen <iamclausius@gmail.com>
2021-08-04 01:22:12 +1000

    add user menu to navbar, log out, apply new style

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,35 +1,140 @@
 import React from 'react';
-import { Flex, Box, Spacer, Link as ChakraLink } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import {
+  Flex,
+  Box,
+  Spacer,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  // useDisclosure,
+  Stack,
+  useColorModeValue,
+  Link as ChakraLink
+} from '@chakra-ui/react';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+// localStorage.clear();
+// localStorage.setItem('token', 'fakeToken');
+// localStorage.setItem('name', 'fakeName');
+
 
 function Navbar() {
+
+  const history = useHistory();
+
+  function AuthTabs() {
+    return (
+      <Stack
+        flex={{ base: 1, md: 0 }}
+        justify={'flex-end'}
+        direction={'row'}
+        spacing={6}>
+        <Button
+          as={Link}
+          to='/login'
+          fontSize={'sm'}
+          fontWeight={400}
+          variant={'link'}
+          href={'#'}>
+          Log in
+        </Button>
+        <Button
+          display={{ base: 'none', md: 'inline-flex' }}
+          fontSize={'sm'}
+          fontWeight={600}
+          color={'white'}
+          bg={'blue.400'}
+          href={'#'}
+          _hover={{
+            bg: 'blue.300',
+
+          }}
+          as={Link}
+          to='/register'>
+          Join
+        </Button>
+      </Stack>
+    )
+
+  }
+
+  function UserTabs() {
+
+    function handleLogOut() {
+      // console.log(localStorage.getItem('token'))
+      const token = localStorage.getItem('token')
+      localStorage.clear();
+      
+      axios.post('/logout',
+      {
+        token: token,
+      }).then(info => {
+        console.log(info);
+        // localStorage.clear();
+      })
+
+      history.push('/')
+      window.location.reload();
+
+  }
+
+    return (
+      // <div>
+      <Flex alignItems={'center'}>
+        <Menu>
+          <MenuButton
+            as={Button}
+            rounded={'full'}
+            variant={'ghost'}
+            cursor={'pointer'}
+            minW={0}>
+            Hi, {localStorage.getItem('name')}
+          </MenuButton>
+          <MenuList
+             minW="0" w={'140px'}
+          >
+            <MenuItem>
+              Profile
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem
+            onClick={handleLogOut}
+            >
+              Log out
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+      // </div>
+    )
+  }
   return (
     <Flex
       id='menu'
-      bg='#2F303A'
-      w='100%'
-      h='8vh'
-      color='white'
-      textAlign='center'
-      margin='auto'
-      fontSize='xl'
+      bg={useColorModeValue('white', 'gray.800')}
+      color={useColorModeValue('gray.600', 'white')}
+      minH={'60px'}
+      py={{ base: 2 }}
+      px={{ base: 4 }}
+      borderBottom={1}
+      borderStyle={'solid'}
+      borderColor={useColorModeValue('gray.200', 'gray.900')}
+      align={'center'}
     >
-      <Box margin='auto' paddingLeft='4%'>
+      <Box margin='auto' paddingLeft='4%' id='logo'>
         <ChakraLink as={Link} to='/'>
           Career Fairs Connect
         </ChakraLink>
       </Box>
       <Spacer />
-      <Flex margin='auto' paddingRight='4%'>
-        <Box paddingRight='50px'>
-          <ChakraLink as={Link} to='/login'>
-            Login
-          </ChakraLink>
-        </Box>
-        <ChakraLink as={Link} to='/register'>
-          Join
-        </ChakraLink>
-      </Flex>
+
+      {localStorage.getItem('token') === null ?
+        <AuthTabs /> : <UserTabs />
+      }
     </Flex>
   );
 }
