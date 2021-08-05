46173bfd36ef2d21041fd7f7895ec2d2c9104407
ThorntonChan <thorntonchan@gmail.com>
2021-08-05 16:02:28 +1000

    1

--- src/core/Logout.py
+++ src/core/Logout.py
@@ -1,10 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 class Logout(APIView):
