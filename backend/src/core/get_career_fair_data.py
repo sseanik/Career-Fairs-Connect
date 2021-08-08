fd841af896d6ad12c82edacb6d76f417578ab1c2
AF <arthur.fung@student.unsw.edu.au>
2021-08-08 17:22:14 +1000

    timestamp hotfix

--- backend/src/core/get_career_fair_data.py
+++ backend/src/core/get_career_fair_data.py
@@ -72,8 +72,8 @@ def get_career_fair_data(request, eventId):
     # rename fields to fit frontend requirements and ordering sakes
     return_dict = {
         "university": career_fair_dict["university_name"],
-        "start": career_fair_dict["start_date"].timestamp(),
-        "end": career_fair_dict["end_date"].timestamp(),
+        "start": career_fair_dict["start_date"],
+        "end": career_fair_dict["end_date"],
         "title": career_fair_dict["title"],
         "description": career_fair_dict["description"],
         "website": career_fair_dict["university_site_url"],
