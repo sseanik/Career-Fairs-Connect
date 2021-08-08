3554c29d7494ae3672f0c0ea2f0753f661a080e2
AF <arthur.fung@student.unsw.edu.au>
2021-08-08 18:52:33 +1000

    get stall

--- backend/src/core/urls.py
+++ backend/src/core/urls.py
@@ -75,7 +75,7 @@ urlpatterns = [
     path('user/data/', userData.as_view()),
 
 
-    path('careerfairs/delete/stalls/<int:stallId>/', delete_stall),
+    path('careerfairs/delete/stalls/', delete_stall),
     path('careerfairs/<int:eventId>/stalls/', StallList.as_view()),
     path('careerfairs/delete/<int:eventId>/', CareerFairListGlobal.as_view()),
     path('careerfairs/applications/', Approvals.as_view()),
