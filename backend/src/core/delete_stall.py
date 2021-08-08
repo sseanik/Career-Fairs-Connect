17bb30a2863a10ff635f613b823266fd93b14c2e
Sean Smith <seaniksmith@gmail.com>
2021-08-08 18:53:18 +1000

    Fixed a problem with __in

--- backend/src/core/delete_stall.py
+++ backend/src/core/delete_stall.py
@@ -12,24 +12,29 @@ from django.utils import timezone
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
+
 @swagger_auto_schema(
     method="delete",
     responses={
-        200 : "Deleted",
-        404 : "Not found",
-        403 : "Forbidden",
+        200: "Deleted",
+        404: "Not found",
+        403: "Forbidden",
     },
     operation_summary="Delete a stall",
     # operation_description="",
 )
-@api_view(['DELETE', ])
+@api_view(
+    [
+        "DELETE",
+    ]
+)
 def delete_stall(request, stallId):
-    if request.user.user_type != "2":
-        return Response({"Forbidden" : "Incorrect user_type"}, status=403)
-    requestUserCompany = Companies.objects.get(user_id = request.user.userID).company_id
-    stall = get_object_or_404(Stalls, pk = stallId)
+    if request.user.user_type != 2:
+        return Response({"Forbidden": "Incorrect user_type"}, status=403)
+    requestUserCompany = Companies.objects.get(user_id=request.user.userID).company_id
+    stall = get_object_or_404(Stalls, pk=stallId)
     stallOwner = stall.company_id
     if requestUserCompany != stallOwner:
-        return Response({"Forbidden" : "Stall does not belong to user"}, status=403)
+        return Response({"Forbidden": "Stall does not belong to user"}, status=403)
     stall.delete()
     return Response("Deleted", status=200)
