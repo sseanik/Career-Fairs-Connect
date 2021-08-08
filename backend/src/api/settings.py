7e330b8bb5cc38cabe7837d59cd3d4150468bd10
Yulia <rodionov.yulia@gmail.com>
2021-08-08 22:19:52 +1000

    updated db connection string

--- backend/src/api/settings.py
+++ backend/src/api/settings.py
@@ -88,24 +88,23 @@ REST_FRAMEWORK = {
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 # For final
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'CareerFairDB',
-#         'USER': 'postgres',
-#         'PASSWORD': 'DPhgwV3xpFGHg8K',
-#         'HOST': 'comp9323.cruw92ayeie9.ap-southeast-2.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CareerFairDB',
+        'USER': 'postgres',
+        'PASSWORD': 'DPhgwV3xpFGHg8K',
+        'HOST': 'comp9323.cruw92ayeie9.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 SWAGGER_SETTINGS = {
    'SECURITY_DEFINITIONS': {
