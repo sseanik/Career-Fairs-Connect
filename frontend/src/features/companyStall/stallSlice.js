2dfc293d1aaee6e011be08d7e5f88cda100dc419
Max Kennedy <max.kennedy03@gmail.com>
2021-08-09 16:40:58 +1000

    Removed stray console.logs

--- frontend/src/features/companyStall/stallSlice.js
+++ frontend/src/features/companyStall/stallSlice.js
@@ -71,9 +71,6 @@ export const asyncEditOpportunity = createAsyncThunk(
 export const asyncDeleteOpportunity = createAsyncThunk(
   'stall/deleteOpportunity',
   async ({ companyID, jobID, toast }) => {
-    console.log(companyID);
-    console.log(jobID);
-
     await axios({
       method: 'delete',
       url: `/company/opportunities/${jobID}/`,
@@ -205,7 +202,6 @@ export const asyncEditQuestion = createAsyncThunk(
 export const asyncAnswerQuestion = createAsyncThunk(
   'stall/answerQuestion',
   async ({ stallId, questionId, answer, toast }) => {
-    console.log('questionID = ' + questionId)
     const response = await axios({
       method: 'put',
       url: `/questions/answer/${stallId}/${questionId}/`,
@@ -292,7 +288,6 @@ export const stallSlice = createSlice({
         state.website = payload.website;
         state.opportunities = payload.opportunities;
         state.events = payload.presentations;
-        console.log(payload.qandas)
         state.qandas = payload.qandas;
         const dominantColourObj = complementaryTextColour(payload.colour);
         state.bgColour = dominantColourObj.bgColour;
@@ -305,7 +300,6 @@ export const stallSlice = createSlice({
       })
       .addCase(asyncAddOpportunity.fulfilled, (state, { payload }) => {
         state.status = false;
-        console.log(payload);
         state.opportunities.push({
           id: payload.job_id,
           type: payload.type,
@@ -323,10 +317,6 @@ export const stallSlice = createSlice({
       })
       .addCase(asyncEditOpportunity.fulfilled, (state, { payload }) => {
         state.formStatus = 'Completed';
-
-        console.log(payload);
-        console.log(current(state).opportunities);
-
         const index = current(state.opportunities).findIndex(
           (opportunity) => opportunity.id === parseInt(payload.job_id)
         );
@@ -346,7 +336,6 @@ export const stallSlice = createSlice({
         state.formStatus = 'Pending';
       })
       .addCase(asyncDeleteOpportunity.fulfilled, (state, { payload }) => {
-        console.log(payload);
         state.formStatus = 'Completed';
         state.opportunities = state.opportunities.filter(
           (opportunity) => opportunity.id !== payload
@@ -408,7 +397,6 @@ export const stallSlice = createSlice({
         state.status = true;
       })
       .addCase(asyncPostQuestion.fulfilled, (state, { payload }) => {
-        console.log(payload)
         state.status = false;
         state.qandas.push({
           id: payload.post_id,
