9af4c66aa958751a22049d1f29121a60c95b345f
Sean Smith <seaniksmith@gmail.com>
2021-08-09 16:16:01 +1000

    Moved edit and delete question to the right

--- frontend/src/features/companyStall/QuestionsAndAnswers.js
+++ frontend/src/features/companyStall/QuestionsAndAnswers.js
@@ -18,184 +18,205 @@ import {
 } from '@chakra-ui/react';
 // Redux
 import { useDispatch, useSelector } from 'react-redux';
-import { asyncAnswerQuestion, asyncDeleteQuestion, asyncPostQuestion } from './stallSlice';
+import {
+  asyncAnswerQuestion,
+  asyncDeleteQuestion,
+  asyncPostQuestion,
+} from './stallSlice';
 import { QuestionModal } from '../../components/QuestionModal';
 import { AnswerModal } from '../../components/AnswerModal';
 
 import { RiPencilFill } from 'react-icons/ri';
 
 export function QuestionsAndAnswers(props) {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [isQuestion, setIsQuestion] = React.useState(true)
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isQuestion, setIsQuestion] = React.useState(true);
   const [question, setQuestion] = React.useState('');
   const [answer, setAnswer] = React.useState('');
   const [id, setId] = React.useState('');
   const buttonLoading = useSelector((state) => state.stall.status);
   const toast = useToast();
   const dispatch = useDispatch();
-  const userId = useSelector((state) => state.user.userID)
-  const companyId = useSelector((state) => state.user.companyID)
+  const userId = useSelector((state) => state.user.userID);
+  const companyId = useSelector((state) => state.user.companyID);
 
   const postQuestion = () => {
     question &&
-      dispatch(asyncPostQuestion({ id: props.stallID,
-        question: {
-          question: question
-        },
-        toast: toast }));
+      dispatch(
+        asyncPostQuestion({
+          id: props.stallID,
+          question: {
+            question: question,
+          },
+          toast: toast,
+        })
+      );
   };
 
   const postAnswer = (id) => {
     answer &&
-      dispatch(asyncAnswerQuestion({ questionId: id, stallId: props.stallID, 
-        answer: {
-          answer: answer
-        }, toast: toast }))
-      .then(setAnswer(''))
-  }
+      dispatch(
+        asyncAnswerQuestion({
+          questionId: id,
+          stallId: props.stallID,
+          answer: {
+            answer: answer,
+          },
+          toast: toast,
+        })
+      ).then(setAnswer(''));
+  };
 
   const deleteQuestion = (id) => {
     id &&
-      dispatch(asyncDeleteQuestion({stallId: props.stallID, postId: id, toast: toast}))
-  }
+      dispatch(
+        asyncDeleteQuestion({
+          stallId: props.stallID,
+          postId: id,
+          toast: toast,
+        })
+      );
+  };
 
   return (
     <div>
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      {isQuestion ?
-      <QuestionModal
-          isOpen={isOpen}
-          stallId={props.stallID}
-          questionId={id}
-          onClose={onClose}
-          question={question}
-          setQuestion={setQuestion}
-      />
-      :
-      <AnswerModal
-        isOpen={isOpen}
-        stallId={props.stallID}
-        questionId={id}
-        onClose={onClose}
-        answer={answer}
-        setAnswer={setAnswer}
-      />
-      }
-    </Modal>
-    <Box>
-      <Text mb='8px' fontWeight='semibold'>
-        Submit your Question:
-      </Text>
-      <Textarea
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-        size='sm'
-      />
-      <Button
-        size='sm'
-        mt='2'
-        mb='4'
-        onClick={() => postQuestion()}
-        isLoading={buttonLoading}
-        loadingText='Submitting'
-        spinnerPlacement='end'
-        colorScheme='green'
-      >
-        Submit
-      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        {isQuestion ? (
+          <QuestionModal
+            isOpen={isOpen}
+            stallId={props.stallID}
+            questionId={id}
+            onClose={onClose}
+            question={question}
+            setQuestion={setQuestion}
+          />
+        ) : (
+          <AnswerModal
+            isOpen={isOpen}
+            stallId={props.stallID}
+            questionId={id}
+            onClose={onClose}
+            answer={answer}
+            setAnswer={setAnswer}
+          />
+        )}
+      </Modal>
+      <Box>
+        <Text mb='8px' fontWeight='semibold'>
+          Submit your Question:
+        </Text>
+        <Textarea
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          size='sm'
+        />
+        <Button
+          size='sm'
+          mt='2'
+          mb='4'
+          onClick={() => postQuestion()}
+          isLoading={buttonLoading}
+          loadingText='Submitting'
+          spinnerPlacement='end'
+          colorScheme='green'
+        >
+          Submit
+        </Button>
 
-      <Accordion allowMultiple>
-        {props.qandas.map((qanda, idx) => (
-          <AccordionItem isDisabled={!qanda.answer && (companyId !== props.companyID)} key={`qanda-${idx}`}>
-            <h2>
-              <AccordionButton>
-                <Box flex='1' textAlign='left' fontWeight='semibold'>
-                  {qanda.question}
-                  {(userId === qanda.author_id) ?
-                    <>
-                      <Button
+        <Accordion allowMultiple>
+          {props.qandas.map((qanda, idx) => (
+            <AccordionItem
+              isDisabled={!qanda.answer && companyId !== props.companyID}
+              key={`qanda-${idx}`}
+            >
+              <Flex align='center'>
+                <AccordionButton>
+                  <Box flex='1' textAlign='left' fontWeight='semibold'>
+                    {qanda.question}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                {userId === qanda.author_id ? (
+                  <>
+                    <Button
                       leftIcon={<RiPencilFill />}
                       size='sm'
                       ml='3'
                       onClick={() => {
                         setQuestion(qanda.question);
                         setId(qanda.id);
-                        setIsQuestion(true)
+                        setIsQuestion(true);
                         onOpen();
                       }}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      colorScheme='red'
+                      size='sm'
+                      ml='3'
+                      onClick={() => {
+                        deleteQuestion(qanda.id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </>
+                ) : (
+                  <> </>
+                )}
+              </Flex>
+              <AccordionPanel pb={4}>
+                {!qanda.answer ? (
+                  <>
+                    <Textarea
+                      value={answer}
+                      onChange={(e) => setAnswer(e.target.value)}
+                      size='sm'
+                    />
+                    <Button
+                      size='sm'
+                      mt='2'
+                      mb='4'
+                      onClick={() => {
+                        postAnswer(qanda.id);
+                      }}
+                      isLoading={buttonLoading}
+                      loadingText='Submitting'
+                      spinnerPlacement='end'
+                    >
+                      Submit
+                    </Button>
+                  </>
+                ) : (
+                  <Flex>
+                    <Box>{qanda.answer}</Box>
+                    {companyId === props.companyID ? (
+                      <Button
+                        leftIcon={<RiPencilFill />}
+                        align='right'
+                        size='sm'
+                        ml='auto'
+                        onClick={() => {
+                          setAnswer(qanda.answer);
+                          setId(qanda.id);
+                          setIsQuestion(false);
+                          onOpen();
+                        }}
                       >
                         Edit
                       </Button>
-                      <Button
-                        colorScheme='red'
-                        size='sm'
-                        ml='3'
-                        onClick={() => {
-                          deleteQuestion(qanda.id);
-                        }}
-                      >
-                        Delete
-                      </Button>
-                    </>
-                   :
-                  <> </>}
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              
-              {!qanda.answer ?
-              <>
-              <Textarea
-                value={answer}
-                onChange={(e) => setAnswer(e.target.value)}
-                size='sm'
-              />
-              <Button
-                size='sm'
-                mt='2'
-                mb='4'
-                onClick={() => {
-                  postAnswer(qanda.id)
-                }}
-                isLoading={buttonLoading}
-                loadingText='Submitting'
-                spinnerPlacement='end'
-              >
-                Submit
-              </Button></>
-            :
-            <Flex>
-              <Box>
-                {qanda.answer}
-              </Box>
-              {(companyId === props.companyID) ?
-                <Button
-                  leftIcon={<RiPencilFill />}
-                  align='right'
-                  size='sm'
-                  ml='auto'
-                  onClick={() => {
-                    setAnswer(qanda.answer);
-                    setId(qanda.id);
-                    setIsQuestion(false)
-                    onOpen();
-                  }}
-                >
-                  Edit
-                </Button>
-                :
-                <> </>
-              }
-            </Flex>
-            }
-             </AccordionPanel>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </Box>
+                    ) : (
+                      <> </>
+                    )}
+                  </Flex>
+                )}
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </Box>
     </div>
   );
 }
